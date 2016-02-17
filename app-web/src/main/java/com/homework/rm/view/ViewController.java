c52169cd7a752416c3fcd156577a101f9c015da2
Константин <exslims1@mail.ru>
2016-02-17 15:33:09 +0400

    added some files for correct deploy

---
+++ app-web/src/main/java/com/homework/rm/view/ViewController.java
@@ -0,0 +1,31 @@
+package com.homework.rm.view;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Константин on 10.01.2016.
+ */
+@Controller
+public class ViewController {
+    private Logger logger = Logger.getLogger(ViewController.class);
+    @RequestMapping("/{module}/")
+    public String getView(@PathVariable("module") String module, HttpServletRequest httpServletRequest, Model model){
+        model.addAllAttributes(httpServletRequest.getParameterMap());
+        model.addAttribute("module",model);
+        logger.trace("Getting " + module + " page");
+        return "view." + module;
+    }
+    @RequestMapping("/")
+    public String getTestView(HttpServletRequest httpServletRequest, Model model){
+        model.addAllAttributes(httpServletRequest.getParameterMap());
+        model.addAttribute("module",model);
+        return "view.test";
+    }
+
+}
